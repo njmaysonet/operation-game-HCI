2594fb0595b0c597f84028c481cadb915f36446d
Mauricio Mendez <mendez1226@gmail.com>
2016-11-20 15:38:23 -0500

    Added Shuffle card script, Can be used with a deck object in order to shuffle order

---
+++ New Unity Project/Assets/Scripts/ShuffleDecks.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ShuffleDecks : MonoBehaviour {
+
+    List<int> cards;
+
+    public void Shuffle()
+    {
+        if (cards == null)
+        {
+            cards = new List<int>();
+        }
+        else
+        {
+            cards.Clear();
+        }
+
+        for(int i = 0; i < 12; i++)//To  be called by both Docter deck and Specialist Deck individually
+        {
+            cards.Add(i);
+        }
+    }
+    void Start()
+    {
+        Shuffle();
+    }
+}
